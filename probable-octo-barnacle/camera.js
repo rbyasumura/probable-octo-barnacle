9d7c4425b950b2c9419787b1c4a849da79068f0d
Bobby Yasumura <byasumura@bdo.ca>
2016-07-25 01:24:32 -0400

    camera implemented

---
+++ probable-octo-barnacle/camera.js
@@ -0,0 +1,50 @@
+﻿var mouse = {
+    x: 0,
+    y: 0,
+    isDown: false,
+}
+var transform = {
+    scale: 1,
+    dx: 0,
+    dy: 0,
+}
+function adjustPan() {
+    transform.dx = Math.max(canvas.width * (1 - transform.scale), Math.min(transform.dx, 0));
+    transform.dy = Math.max(canvas.height * (1 - transform.scale), Math.min(transform.dy, 0));
+}
+
+document.addEventListener("mousewheel", function (e) {
+    var e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+
+    transform._scale = transform.scale + delta / 10;
+    transform._scale = Math.max(1, transform._scale);
+
+    transform.dx -= e.offsetX;
+    transform.dx *= transform._scale / transform.scale;
+    transform.dx += e.offsetX;
+    transform.dy -= e.offsetY;
+    transform.dy *= transform._scale / transform.scale;
+    transform.dy += e.offsetY;
+
+    transform.scale = transform._scale;
+
+    adjustPan();
+}, false);
+canvas.addEventListener("mousedown", function (e) {
+    mouse.isDown = true;
+    mouse.x = e.x;
+    mouse.y = e.y;
+    transform._dx = transform.dx;
+    transform._dy = transform.dy;
+}, false);
+document.addEventListener("mousemove", function (e) {
+    if (mouse.isDown) {
+        transform.dx = transform._dx + (e.x - mouse.x);
+        transform.dy = transform._dy + (e.y - mouse.y);
+        adjustPan();
+    }
+}, false);
+document.addEventListener("mouseup", function (e) {
+    mouse.isDown = false;
+}, false);
---
+++ probable-octo-barnacle/gaussian.js
@@ -0,0 +1,20 @@
+﻿var spare;
+var isSpareReady = false;
+
+function getGaussian(mean, stdDev) {
+    if (isSpareReady) {
+        isSpareReady = false;
+        return spare * stdDev + mean;
+    } else {
+        var u, v, s = 0;
+        for (; s >= 1 || s == 0;) {
+            u = Math.random() * 2 - 1;
+            v = Math.random() * 2 - 1;
+            s = u * u + v * v;
+        }
+        var mul = Math.sqrt(-2 * Math.log(s) / s);
+        spare = v * mul;
+        isSpareReady = true;
+        return mean + stdDev * u * mul;
+    }
+}
